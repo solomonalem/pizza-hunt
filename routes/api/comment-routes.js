4243db09cf2b0dbd2e0c7c6009bb0b2a96d8ca58
Solomon <solomonmeresa8@gmail.com>
2020-11-19 20:52:10 -0800

    adds replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
 
 
 // /api/comments/<pizzaId>
@@ -9,7 +14,13 @@ router
 
 // /api/comments/<pizzaId>/<commentId>
 router
-.route('/:pizzaId/:commentId')
-.delete(removeComment);
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
+
+
+router
+.route('/:pizzaId/:commentId/:replyId')
+.delete(removeReply);
 
 module.exports = router;
